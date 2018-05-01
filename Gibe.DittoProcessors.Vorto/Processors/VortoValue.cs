e44c9115467dd18507197b0f438538db94d9c2c8
Tristan Thompson <tristan.thompson@gibedigital.com>
2018-05-01 10:39:28 +0100

    Refactor VortoValue to allow passing of more options; recursive, default value, etc..

--- Gibe.DittoProcessors.Vorto/Processors/VortoValue.cs
+++ Gibe.DittoProcessors.Vorto/Processors/VortoValue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Gibe.DittoProcessors.Processors;
 using Gibe.DittoProcessors.Vorto.Services;
 using Our.Umbraco.Vorto.Extensions;
@@ -8,25 +9,28 @@ namespace Gibe.DittoProcessors.Vorto.Processors
 {
 	public class VortoValue : InjectableProcessorAttribute
 	{
+		public string CultureName { get; set; }
+		public bool Recursive { get; set; }
+		public object DefaultValue { get; set; }
+		public string FallbackCultureName { get; set; }
+
 		private readonly string _propertyAlias;
-		private const string DefaultLanguageCode = "en-GB";
 		private readonly Func<ILanguageDetectionService> LanguageDetectionService = Inject<ILanguageDetectionService>();
 
-		public VortoValue(string propertyAlias)
+		public VortoValue(string propertyAlias, string cultureName = null, bool recursive = false, object defaultValue = null, string fallbackCultureName = null)
 		{
 			_propertyAlias = propertyAlias;
+			CultureName = cultureName ?? LanguageDetectionService().LanguageCode();
+			Recursive = recursive;
+			DefaultValue = defaultValue;
+			FallbackCultureName = fallbackCultureName ?? ConfigurationManager.AppSettings["Gibe.DittoProcessors.Vorto:FallbackCultureName"] ?? "en-GB"; ;
 		}
 
 		public override object ProcessValue()
 		{
-			var value = (IPublishedContent)Value;
+			var content = (IPublishedContent)Value;
 
-			var languageCode = LanguageDetectionService().LanguageCode();
-
-			var vortoValue = value.GetVortoValue(_propertyAlias, languageCode);
-
-			return value.GetVortoValue(_propertyAlias, vortoValue == null ? DefaultLanguageCode : languageCode);
+			return content.GetVortoValue(_propertyAlias, CultureName, Recursive, DefaultValue, FallbackCultureName);
 		}
-
 	}
 }
